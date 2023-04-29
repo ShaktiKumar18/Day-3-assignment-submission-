ad544f372d2a2647bbb6d98fdb5a5345a6b74e7f
ShaktiKumar18 <132076536+ShaktiKumar18@users.noreply.github.com>
2023-04-29 20:07:10 +0530

    Add files via upload

---
+++ Day 3 assignment.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ") 
+current_age = int(input("Enter your age: ")) 
+#calculating the 100th year, considering 2020 as the current year
+hundredth_year = 2020 + (100 - current_age)
+print(f'{name} will become 100 years old in the year {hundredth_year}.')
